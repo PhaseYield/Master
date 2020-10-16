08771acafa20ed6524cefe1bc4b132535f0507ce
PhaseYield <72132197+PhaseYield@users.noreply.github.com>
2020-10-16 15:30:29 +0100

    refactor test

--- tests/exchange/test_ERC20.py
+++ tests/exchange/test_ERC20.py
@@ -1,10 +1,10 @@
-def test_ERC20(w3, HAY_token, pad_bytes32):
+def test_ERC20(w3, PHY_token, pad_bytes32):
     a0, a1 = w3.eth.accounts[:2]
     assert PHY_token.name() == pad_bytes32('HAY Token')
     assert PHY_token.symbol() == pad_bytes32('HAY')
     assert PHY_token.decimals() == 18
-    assert PHY_token.totalSupply() == 100000*10**18
-    assert PHY_token.balanceOf(a0) == 100000*10**18
+    assert PHY_token.totalSupply() == 1000*10**3
+    assert PHY_token.balanceOf(a0) == 1000*10**3
     PHY_token.transfer(a1, 1*10**18, transact={})
-    assert PHY_token.balanceOf(a0) == 100000*10**18 - 1*10**18
-    assert PHY_token.balanceOf(a1) == 1*10**18
+    assert PHY_token.balanceOf(a0) == 1000*10**3 - 1*10**3
+    assert PHY_token.balanceOf(a1) == 1*10**3
